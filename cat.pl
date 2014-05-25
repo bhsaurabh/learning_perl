60fc69b47e4456294b703f8a633f59c03dd6d018
def <srbhatia10@gmail.com>
2014-05-25 21:58:36 +0530

    Make cat.pl compatible for directories

--- cat.pl
+++ cat.pl
@@ -4,9 +4,21 @@
 use strict;
 use warnings;
 
+sub print_dir {
+  $dir = shift;
+  @files = glob "$dir/*";
+  while (my $file = shift @files) {
+    print "$file\n";
+  }
+}
+
 # definition of a sub-routine should be before it is invoked
 sub print_file {
   my $file = shift;  # get the filename from the parameters
+  # if this is a directory, print its contents
+  if (-d $file) {
+    print_dir $file;
+  }
   open my $fh, $file;
   while (<$fh>) {
     print "$_";  # NOTE: $_ is global, changing it here affects cat
@@ -16,7 +28,7 @@ sub print_file {
 sub cat {
   # when a variable is not provided, foreach uses $_
   foreach (@_) {
-    print_file $_;  
+    print_file $_;
   }
 }
 
