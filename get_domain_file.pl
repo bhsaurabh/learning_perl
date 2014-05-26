31f8220cf0be9dd731d138f0de8aa4141abcd6d5
def <srbhatia10@gmail.com>
2014-05-26 21:52:52 +0530

    Add script to separate domain & fetched file from a URL

---
+++ get_domain_file.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub get_domain_file {
+    # breaks a URL to obtain the domain and the file that is fetched
+    my $pattern = "(http|ftp)://(.*)/(.*)";
+    for my $url (@_) {
+        if ($url =~ m/$pattern/) {
+            print "*******\nURL: $url\nDomain: $2\nFile:$3\n\n";
+        }
+    }
+}
+
+get_domain_file @ARGV;
