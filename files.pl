d7e7c2d5c9800f836f551368ee45220d0f621c2a
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2014-05-26 12:39:24 +0000

    Just a couple of minor tips

--- files.pl
+++ files.pl
@@ -5,7 +5,7 @@ use warnings;
 sub files {
   # open a file
   #open FILE, "/usr/share/dict/words"; # this is a global file handler
-  open my $fh, "/usr/share/dict/words";
+  open my $fh, "<", "/usr/share/dict/words" or die "Can't open: $!";
 
   # In a scalar context, <> reads a line
   my $first = <$fh>;
