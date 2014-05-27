cb956b060e76e9a6d15330779885c2e3d44cf6f1
def <srbhatia10@gmail.com>
2014-05-27 08:21:11 +0530

    Add script to reverse a list in-place

---
+++ reverse_list_inplace.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub swap {
+    ($_[0], $_[1]) = ($_[1], $_[0]);
+}
+
+
+sub reverse_list_inplace {
+    print "Original List: @_\n";
+    for (my $i = 0; $i < @_/2; $i++) {
+        swap ($_[$i], $_[-$i-1]);
+    }
+    print "New List: @_\n";
+}
+
+reverse_list_inplace @ARGV;
